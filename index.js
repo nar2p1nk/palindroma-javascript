60f95a19fb57b834f51f12b5f13901a929ab36b5
nar2p1nk <farrisisqandar@gmail.com>
2022-02-06 16:29:36 +0800

    begin,completed comments

---
+++ index.js
@@ -0,0 +1,5 @@
+// create an app that checks if the word is a palindrome
+// example:
+// if the word is "racecar" then it would turn true since "racecar" backwards is still "racecar"
+// if the word is "hill" then it would return false since "hill" backwards is "llih"
+ 
