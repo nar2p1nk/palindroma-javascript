230e4f904147a0571ea4e7cee6efd521862c70a6
nar2p1nk <farrisisqandar@gmail.com>
2022-02-06 16:36:34 +0800

    completed

--- index.js
+++ index.js
@@ -2,4 +2,21 @@
 // example:
 // if the word is "racecar" then it would turn true since "racecar" backwards is still "racecar"
 // if the word is "hill" then it would return false since "hill" backwards is "llih"
+
+
  
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question('enter a word: ', word =>{
+    palindroma(word)
+    readline.close()
+})
+
+const palindroma = async word =>{
+    reversedWord = word.split('').reverse().join('')
+    if(word === reversedWord){console.log('it is a palindrome!')}
+    else{console.log('this is not a palindrome')}
+}
